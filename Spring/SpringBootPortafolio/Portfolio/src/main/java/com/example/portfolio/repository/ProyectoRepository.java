edf91bd9cc46a493a6f1f30d6657999a15b293be
Enrique <dakab7h@gmail.com>
2025-06-17 14:07:38 +0200

    Delete Spring directory

--- Spring/SpringBootPortafolio/Portfolio/src/main/java/com/example/portfolio/repository/ProyectoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.portfolio.repository;
-
-import com.example.portfolio.model.Proyecto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProyectoRepository extends JpaRepository <Proyecto, Long>{}
